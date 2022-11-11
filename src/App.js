40869daa6cbd5b7a8dddd457d3ea30522a0647a9
ceruleanlights <arestus2@gmail.com>
2022-11-11 18:24:48 +0100

    adds few changes and also turn-on prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import './App.css';
-import Profile from './components/Profile/Profile';
+import React from "react";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import data from "./data/user.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Profile/>
-      </header>
-    </div>
-  );
+   const {
+      username,
+      tag,
+      location,
+      avatar,
+      stats: { followers, views, likes },
+   } = data;
+
+   return (
+      <div className="App">
+         <header className="App-header">
+            <Profile
+               username={username}
+               tag={tag}
+               location={location}
+               avatar={avatar}
+               followers={followers}
+               views={views}
+               likes={likes}
+            />
+         </header>
+      </div>
+   );
 }
 
 export default App;
